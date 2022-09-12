bd653f7db2eb8b5a7e00e241ec8557210dd14840
Chinedum <chinedumike85@gmail.com>
2022-09-12 18:44:09 +0100

    navbar and basic app layout done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link} from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 
 import Navbar from './components/Navbar'
+import Homepage from './components/Homepage'
+import Exchanges from './components/Exchanges'
+import Cryptocurrencies from './components/Cryptocurrencies'
+import CryptoDetails from './components/CryptoDetails'
+import News from './components/News'
+
 
 import './App.css';
 
@@ -13,10 +19,30 @@ const App = () => {
         <Navbar/>
       </div>
       <div className='main' >
-
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<Homepage />} /> 
+              <Route path="/exchanges" element={<Exchanges />} /> 
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} /> 
+              <Route path="/coinId" element={<CryptoDetails />} /> 
+              <Route path="/news" element={<News />} /> 
+            </Routes>
+          </div>
+        </Layout>
+        <div className='footer' >
+          <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021
+            <Link to="/">
+              Cryptoverse Inc.
+            </Link> <br />
+            All Rights Reserved.
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
       </div>
-      <div className='footer' >
-
       </div>
     </div>
   )
