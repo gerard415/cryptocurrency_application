f94b69e4696953c2d53032a03bf95a4e3ada30a9
Chinedum <chinedumike85@gmail.com>
2022-09-12 16:53:43 +0100

    very basic unstyled navbar done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import Navbar from './components/Navbar'
+
+import './App.css';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='app' >
+      <div className='navbar' >
+        <Navbar/>
+      </div>
+      <div className='main' >
+
+      </div>
+      <div className='footer' >
+
+      </div>
+    </div>
   )
 }
 
